724be9ee691b8b2fb6e70fa89529dfe1d299cb47
leonardocanone <leonardocanone@gmail.com>
2023-12-01 04:07:23 -0300

    Aula de Typescript 2

--- src/fixacao/exercicio1/index.ts
+++ src/fixacao/exercicio1/index.ts
@@ -4,8 +4,57 @@
   b. idade, que é um número;
   c. corFavorita, que é uma string.
 
-2. Crie mais três objetos, que também precisam ter apenas os campos definidos acima. Crie um type Pessoa para garantir
-que todos os objetos tenham as mesmas propriedades.
+2. Crie mais três objetos, que também precisam ter apenas os campos definidos acima. Crie um type Pessoa para garantir que todos os objetos tenham as mesmas propriedades.
 
 3. Modifique o type Pessoa para que possamos escolher apenas entre as cores do arco-íris. Utilize um enum para isso.
 */
+
+enum COR_FAVORITA {
+  VERMELHO = "vermelho",
+  LARANJA = "laranja",
+  AMARELO = "amarelo",
+  VERDE = "verde",
+  AZUL = "azul",
+  ANIL = "anil",
+  VIOLETA = "violeta"
+}
+
+type TPerson2 = {
+  name: string,
+  idade: number,
+  corFavorita: COR_FAVORITA
+}
+
+const pessoa1: TPerson2 = {
+  name: "Leo",
+  idade: 35,
+  corFavorita: COR_FAVORITA.VERDE
+}
+
+const pessoa2: TPerson2 = {
+  name: "Nena",
+  idade: 35,
+  corFavorita: COR_FAVORITA.VIOLETA
+}
+
+const pessoa3: TPerson2 = {
+  name: "Véia",
+  idade: 61,
+  corFavorita: COR_FAVORITA.AZUL
+}
+
+const pessoa4: TPerson2 = {
+  name: "Véio",
+  idade: 65,
+  corFavorita: COR_FAVORITA.VERDE
+}
+
+
+const ArrayPessoas: TPerson2[] = [
+  pessoa1,
+  pessoa2,
+  pessoa3,
+  pessoa4
+]
+
+console.log(ArrayPessoas);
